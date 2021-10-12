397ccc03346665ea8423cad17e162f020e74fc46
carlosmoncho <carlosmonchomolla@gmail.com>
2021-10-12 11:58:08 +0200

    [ADD] Practica_3.1

---
+++ config/categories.php
@@ -0,0 +1,12 @@
+<?php
+$categories = [
+    [
+        'id'  => '1',
+        'name' => 'computer',],
+    [
+        'id'  => '2',
+        'name' => 'tablet',],
+    [
+        'id'  => '3',
+        'name' => 'mobile',]
+    ];
---
+++ src/Exceptions/NoFitField.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BatoiPOP\Exceptions;
+
+class NoFitField
+{
+
+}
